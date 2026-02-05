644d752e91e682b9f6986cdd6b47a754167ebdd0
Mohitrajput16 <mohitrajput1694@gmail.com>
2026-02-05 13:28:54 +0530

    Fix product controller naming and imports

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,5 @@
 // server/controllers/productController.js
-import asyncHandler from 'async-handler';
+import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 // @desc    Fetch all products
