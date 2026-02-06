2be3ea55de6939e0e130d1ecf94acdc5b0e3eba7
Mohitrajput16 <mohitrajput1694@gmail.com>
2026-02-06 13:31:09 +0530

    Fix user controller token issue

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,8 @@
-// server/controllers/userController.js
 import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken'; 
-import asyncHandler from 'express-async-handler'; // Ensure this package is installed
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
-// --- 1. INTERNAL TOKEN GENERATOR (No external file needed) ---
+// --- INTERNAL TOKEN GENERATOR ---
 const generateTokenInternal = (id) => {
   return jwt.sign({ userId: id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
@@ -12,130 +11,132 @@ const generateTokenInternal = (id) => {
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
-// @access  Public
-const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+const authUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-  const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      const token = generateTokenInternal(user._id);
 
-  if (user && (await user.matchPassword(password))) {
-    
-    // --- 2. GENERATE TOKEN HERE ---
-    const token = generateTokenInternal(user._id);
-
-    // --- 3. SEND TOKEN IN RESPONSE ---
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: token, // <--- This MUST be here
-    });
-  } else {
-    res.status(401);
-    throw new Error('Invalid email or password');
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: token, // <--- Guaranteed to send now
+      });
+    } else {
+      res.status(401);
+      throw new Error('Invalid email or password');
+    }
+  } catch (error) {
+    next(error);
   }
-});
+};
 
 // @desc    Register user
 // @route   POST /api/users
-// @access  Public
-const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
 
-  const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
 
-  if (userExists) {
-    res.status(400);
-    throw new Error('User already exists');
+    const user = await User.create({ name, email, password });
+
+    if (user) {
+      const token = generateTokenInternal(user._id);
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: token,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+  } catch (error) {
+    next(error);
   }
-
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
-
-  if (user) {
-    const token = generateTokenInternal(user._id);
-
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: token, // <--- AND HERE
-    });
-  } else {
-    res.status(400);
-    throw new Error('Invalid user data');
-  }
-});
+};
 
 // @desc    Get user profile
-// @route   GET /api/users/profile
-// @access  Private
-const getUserProfile = async (req, res) => {
-  const user = await User.findById(req.user._id);
-
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
+const getUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
+    }
+  } catch (error) {
+    next(error);
   }
 };
 
 // @desc    Update user profile
-// @route   PUT /api/users/profile
-// @access  Private
-const updateUserProfile = async (req, res) => {
-  const user = await User.findById(req.user._id);
-
-  if (user) {
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-
-    if (req.body.password) {
-      user.password = req.body.password;
+const updateUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+      const updatedUser = await user.save();
+      const token = generateTokenInternal(updatedUser._id);
+      
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: token,
+      });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
     }
-
-    const updatedUser = await user.save();
-    
-    // Send new token on update too, just in case
-    const token = generateTokenInternal(updatedUser._id);
-
-    res.json({
-      _id: updatedUser._id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-      isAdmin: updatedUser.isAdmin,
-      token: token, 
-    });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
+  } catch (error) {
+    next(error);
   }
 };
 
-const getUsers = async (req, res) => {
-  const users = await User.find({});
-  res.json(users);
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteUser = async (req, res) => {
-  const user = await User.findById(req.params.id);
-  if (user) {
-    await user.deleteOne();
-    res.json({ message: 'User removed' });
-  } else {
-    res.status(404);
-    throw new Error('User not found');
+const deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      await user.deleteOne();
+      res.json({ message: 'User removed' });
+    } else {
+      res.status(404);
+      throw new Error('User not found');
+    }
+  } catch (error) {
+    next(error);
   }
 };
 
